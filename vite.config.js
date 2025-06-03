9de3d746be486a8fa66c4a3381e5cab8d12a3196
41Uday <uday1729vv@gmail.com>
2025-06-03 20:35:51 +0530

    Hot fix: correcged the urls in both the files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/41Uday/TodoList',
+  base: '/TodoList',
   plugins: [react()],
 });
